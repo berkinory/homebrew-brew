86c4a6c8fe236c4fc22ab997fbc823c5b9c46e62
berkinory <berk@mirac.dev>
2026-09-24 22:48:48 +0300

    Use canonical Glade repository URL

--- Casks/glade.rb
+++ Casks/glade.rb
@@ -2,10 +2,10 @@ cask "glade" do
   version "0.1.0"
   sha256 "0de72783374cc929e4b8fd5d041b4bef119777dc0bdc1f4249a9bf4249dc4c77"
 
-  url "https://github.com/berkinory/glade/releases/download/v#{version}/Glade.dmg"
+  url "https://github.com/berkinory/Glade/releases/download/v#{version}/Glade.dmg"
   name "Glade"
   desc "Web browser based on Search"
-  homepage "https://github.com/berkinory/glade"
+  homepage "https://github.com/berkinory/Glade"
 
   depends_on macos: :sonoma
 
