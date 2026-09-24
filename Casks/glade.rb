38a984331928d7baa6d1739153000c8c4f2c3036
berkinory <berk@mirac.dev>
2026-09-24 22:45:41 +0300

    Add Glade cask

---
+++ Casks/glade.rb
@@ -0,0 +1,13 @@
+cask "glade" do
+  version "0.1.0"
+  sha256 "0de72783374cc929e4b8fd5d041b4bef119777dc0bdc1f4249a9bf4249dc4c77"
+
+  url "https://github.com/berkinory/glade/releases/download/v#{version}/Glade.dmg"
+  name "Glade"
+  desc "Web browser based on Search"
+  homepage "https://github.com/berkinory/glade"
+
+  depends_on macos: :sonoma
+
+  app "Glade.app"
+end
